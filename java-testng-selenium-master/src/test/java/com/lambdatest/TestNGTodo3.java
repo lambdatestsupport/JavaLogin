d5284e23b7eaa638c7affbd9e79bed40c64632e6
rlambdatest <rakeshs@lambdatest.com>
2023-08-09 13:11:11 +0530

    first commit

---
+++ java-testng-selenium-master/src/test/java/com/lambdatest/TestNGTodo3.java
@@ -0,0 +1,71 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTodo3 {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        ;
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "MacOS Catalina");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "latest");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", m.getName() + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+
+        String[] Tags = new String[] { "Feature", "Tag", "Moderate" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+        String spanText;
+        System.out.println("Loading Url");
+//        Thread.sleep(100);
+//        driver.get("https://lambdatest.github.io/sample-todo-app/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        // Enter your login email id
+        driver.findElement(By.id("email")).sendKeys("shubhamr@lambdatest.com");
+
+        // Enter your login password
+        driver.findElement(By.id("password")).sendKeys("Gmail@123");
+        driver.findElement(By.id("login-button")).sendKeys(Keys.ENTER);
+
+        Thread.sleep(20000);
+        driver.quit();
+        Thread.sleep(800);
+
+        System.out.println("TestFinished");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
